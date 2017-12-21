e7cf4db9aab3315cf22ae87926af1e1e17934b68
JackChen <17611226590@163.com>
2017-12-21 12:51:59 +0800

    创建了Model.java

---
+++ Model.java
@@ -0,0 +1,41 @@
+package cn.itcast.domain;
+
+public class Model {
+	private String outUser;
+	private String inUser;
+	private int money;
+	public Model() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Model(String outUser, String inUser, int money) {
+		super();
+		this.outUser = outUser;
+		this.inUser = inUser;
+		this.money = money;
+	}
+	public String getOutUser() {
+		return outUser;
+	}
+	public void setOutUser(String outUser) {
+		this.outUser = outUser;
+	}
+	public String getInUser() {
+		return inUser;
+	}
+	public void setInUser(String inUser) {
+		this.inUser = inUser;
+	}
+	public int getMoney() {
+		return money;
+	}
+	public void setMoney(int money) {
+		this.money = money;
+	}
+	@Override
+	public String toString() {
+		return "Model [outUser=" + outUser + ", inUser=" + inUser + ", money=" + money + "]";
+	}
+	
+
+}
